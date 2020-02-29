d3c159dd8e5e9e0d000f08a33642d6c03fc329a4
ChrisJu <chris-ju@qq.com>
2020-02-29 16:42:24 +0800

    Analyse Information

--- controller/evaluate.go
+++ controller/evaluate.go
@@ -2,6 +2,8 @@ package controller
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 
 	"github.com/Penetration-Platform-Go/Admin-Service/lib"
 	"github.com/Penetration-Platform-Go/Admin-Service/model"
@@ -15,18 +17,49 @@ func robotEvaluate(project *model.Project) (int, string) {
 		network = append(network, each.Connected)
 	}
 
-	lib.InitMatrix(network)
+	wrongIP := checkEquipmentIPFormat(project.Equipment)
+	wrongEquipmentPair := getConnectedEquipmentPairWithWrongIP(project.Equipment, network)
 
+	lib.InitMatrix(network)
 	deep := getNetworkDeep(network)
 	equipmentTypeNumber := getPCNumber(project.Equipment)
 	childNetwrok := getChildNetworkNumber(network)
 
-	information := fmt.Sprintf("Network Deep:\t %d\n", deep)
+	information := ""
+
+	if len(wrongIP) != 0 {
+		information += fmt.Sprintf("%d Equipments' ip maybe wrong\n", len(wrongIP))
+		for equipmentIndex := range wrongIP {
+			information += fmt.Sprintf("%s: %s\n", project.Equipment[equipmentIndex].Name, wrongIP[equipmentIndex])
+		}
+	}
+
+	wrongPairNumber := 0
+	wrongPairInformation := ""
+	for equipmentIndex := range wrongEquipmentPair {
+		_, exists := wrongIP[equipmentIndex]
+		if exists {
+			continue
+		}
+		wrongPairNumber++
+		wrongPairInformation += fmt.Sprintf("%s: %s <-> %s: %s\n",
+			project.Equipment[equipmentIndex].Name,
+			strings.Join(project.Equipment[equipmentIndex].Ip, ","),
+			project.Equipment[wrongEquipmentPair[equipmentIndex]].Name,
+			strings.Join(project.Equipment[wrongEquipmentPair[equipmentIndex]].Ip, ","))
+	}
+	if wrongPairNumber != 0 {
+		information += fmt.Sprintf("\n%d pairs equipments are connected but those IP aren't in same network mask\n", wrongPairNumber)
+		information += wrongPairInformation
+	}
+
+	information += fmt.Sprintf("\nNetwork Deep:\t %d\n", deep)
+	information += "\nAll Type Equipment Number\n"
 	for t := range equipmentTypeNumber {
 		information += fmt.Sprintf("%s Number:\t %d\n", t, equipmentTypeNumber[t])
 	}
 
-	information += fmt.Sprintf("Child Network Number:\t %d\n", childNetwrok)
+	information += fmt.Sprintf("\nChild Network Number:\t %d\n\n", childNetwrok)
 
 	return 0, information
 }
@@ -61,4 +94,79 @@ func getNetworkDeep(network [][]bool) int {
 		}
 	}
 	return deep
+}
+
+func checkEquipmentIPFormat(equipmentList []*pb.Equipment) map[int]string {
+	var result map[int]string
+	result = make(map[int]string)
+	for index, each := range equipmentList {
+		for _, ip := range each.Ip {
+			if !checkIPFormat(ip) {
+				result[index] = ip
+			}
+		}
+	}
+	return result
+}
+
+func checkIPFormat(ipString string) bool {
+	var ipMap map[uint32]bool
+	ipMap = map[uint32]bool{10: true, 100: true, 101: true, 172: true, 192: true, 127: true}
+	ip, _ := getIPandMask(ipString)
+	A := ip >> 24
+	_, exists := ipMap[A]
+	if !exists {
+		return false
+	}
+	return true
+}
+
+func getConnectedEquipmentPairWithWrongIP(equipmentList []*pb.Equipment, network [][]bool) map[int]int {
+	var result map[int]int
+	result = make(map[int]int)
+
+	count := len(network)
+	for i := 0; i < count; i++ {
+		for j := 0; j < count; j++ {
+			if i == j {
+				continue
+			} else if network[i][j] {
+				connected := checkIPConnected(equipmentList[i].Ip, equipmentList[j].Ip)
+				if !connected {
+					result[i] = j
+				}
+			}
+		}
+	}
+	return result
+}
+
+func checkIPConnected(AIP []string, BIP []string) bool {
+	// TODO: 先将 IP 初始化
+	for i := 0; i < len(AIP); i++ {
+		aip, amask := getIPandMask(AIP[i])
+		for j := 0; j < len(BIP); j++ {
+			bip, bmask := getIPandMask((BIP[j]))
+			if aip>>uint((32-amask)) == bip>>uint((32-bmask)) {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func getIPandMask(ip string) (uint32, int) {
+	ipAndMask := strings.Split(ip, "/")
+	ipList := strings.Split(ipAndMask[0], ".")
+	mask := 24
+	var result uint32
+	result = 0
+	if len(ipAndMask) == 2 {
+		mask, _ = strconv.Atoi(ipAndMask[1])
+	}
+	for index, each := range ipList {
+		temp, _ := strconv.Atoi(each)
+		result += (uint32(temp) << uint(((3 - index) * 8)))
+	}
+	return result, mask
 }
